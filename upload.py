8c60f2cddd42f637312266a265557ba001951408
genmo1209 <genmo1209@gmail.com>
2026-02-17 23:31:09 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -127,7 +127,7 @@ if not files:
     print("No videos found.")
     exit()
 
-videos_to_upload = files[:4]   # 2 videos per run
+videos_to_upload = files[:1]   # 2 videos per run
 
 # ==============================
 # MAIN LOOP
